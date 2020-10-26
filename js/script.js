a1fdd13f67af90fede83a06ee35c6ddaf8fc0346
Hlibov <gglebov777@gmail.com>
2020-10-26 02:29:08 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -15,12 +15,27 @@
 'use strict';
 
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+  movies: [
+    "Логан",
+    "Лига справедливости",
+    "Ла-ла лэнд",
+    "Одержимость",
+    "Скотт Пилигрим против..."
+  ]
 };
 
+const sortedArray = movieDB.movies.sort();
+const adUnits = document.querySelectorAll('.promo__adv img');
+let genre = document.body.getElementsByClassName('promo__genre');
+let bg = document.body.querySelector('.promo__bg');
+let li = document.body.querySelectorAll('.promo__interactive-item');
+
+adUnits.forEach(el => {
+  el.remove();
+});
+
+genre[0].innerHTML = 'драма';
+bg.style.background = 'url(../img/bg.jpg) center top/cover no-repeat';
+li.forEach((el, i) => {
+  el.innerHTML = `${i + 1}. ${sortedArray[i]}`;
+});
